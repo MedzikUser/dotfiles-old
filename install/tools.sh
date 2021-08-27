00cea0be1cd64be16ea7b2f3bcf40d730dbc47c2
Medzik <87065584+MedzikUser@users.noreply.github.com>
2021-08-27 15:45:48 +0200

    update

--- install/tools.sh
+++ install/tools.sh
@@ -7,14 +7,15 @@ go install golang.org/x/tools/gopls@latest \
 	github.com/uudashr/gopkgs/cmd/gopkgs@v2 \
 	github.com/fatih/gomodifytags@latest \
 	github.com/josharian/impl@latest \
-	honnef.co/go/tools/cmd/staticcheck@latest
+	honnef.co/go/tools/cmd/staticcheck@latest \
+	github.com/ramya-rao-a/go-outline@latest
+
+## Other tools
+curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
 
 # Clean
 go clean
 rm -rf $GOPATH/src $GOPATH/pkg
-
-## Other tools
-curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
 
 
 # Node.js
